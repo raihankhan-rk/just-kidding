2cdfff2a22e8947c9b10e2c5490955a947d7554c
Raihan Khan <iamrk98@gmail.com>
2023-03-15 07:55:43 +0530

    added cors support

--- server.py
+++ server.py
@@ -1,17 +1,23 @@
 from utils.helper import parse_json
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 from datetime import datetime, date
 from db.db import db
 
 app = Flask(__name__)
+cors = CORS(app)
 now = datetime.now()
 today = date.today()
 
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route("/")
+@cross_origin()
 def home():
     return jsonify({"status": 200, "message": "Server Up and running!"})
 
 @app.route("/post-joke", methods=["POST"])
+@cross_origin()
 def post_joke():
     try:
         data = request.json
@@ -20,11 +26,12 @@ def post_joke():
         db.jokes.insert_one(parse_json(data))
         print(data)
         # return data
-        return "Voila! You just posted a joke on Just-kidding! Check it out on https://just-kidding.raihankhan.dev"
+        return "Voila! You just posted a joke on Just-kidding! Check it out on https://just-kidding.vercel.app"
     except:
         return "Oops! Some Error occured. you might wanna recheck your json data once again."
 
 @app.route("/get-jokes", methods=["GET"])
+@cross_origin()
 def get_jokes():
     data = db.jokes.find()
     return parse_json(data)
